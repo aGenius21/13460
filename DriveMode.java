bf557ebc13a377be1638ad640598471315fef1ca
aGenius21 <97985830+aGenius21@users.noreply.github.com>
2022-01-31 14:45:41 -0600

    Update DriveMode.java
     revert to a standard mecanum drive base program

--- DriveMode.java
+++ DriveMode.java
@@ -10,11 +10,8 @@ public class DriveMode extends LinearOpMode {
 
   private DcMotor rearleft;
   private DcMotor frontright;
-  private DcMotor armMotor;
   private DcMotor frontleft;
   private DcMotor rearright;
-  private DcMotor fingerMotor;
-  private DcMotor wheelMotor;
 
   @Override
   public void runOpMode() {
@@ -25,17 +22,13 @@ public class DriveMode extends LinearOpMode {
 
     rearleft = hardwareMap.get(DcMotor.class, "rearleft");
     frontright = hardwareMap.get(DcMotor.class, "frontright");
-    armMotor = hardwareMap.get(DcMotor.class, "armMotor");
     frontleft = hardwareMap.get(DcMotor.class, "frontleft");
     rearright = hardwareMap.get(DcMotor.class, "rearright");
-    fingerMotor = hardwareMap.get(DcMotor.class, "fingerMotor");
-    wheelMotor = hardwareMap.get(DcMotor.class, "wheelMotor");
 
     drivePower = 0.6;
     rearleft.setDirection(DcMotorSimple.Direction.REVERSE);
     frontright.setDirection(DcMotorSimple.Direction.REVERSE);
-    armMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-    armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    
     waitForStart();
     if (opModeIsActive()) {
       while (opModeIsActive()) {
@@ -46,15 +39,10 @@ public class DriveMode extends LinearOpMode {
         rearright.setPower((forward - strafe) + turn);
         rearleft.setPower((forward + strafe) - turn);
         frontright.setPower((forward - strafe) - turn);
-        armMotor.setPower(-(0.5 * gamepad2.right_stick_y));
         if (gamepad1.dpad_up) {
           if (drivePower < 1) {
             drivePower += 0.1;
           }
-        } else if (gamepad2.x) {
-          fingerMotor.setPower(0.5);
-        } else if (gamepad2.b) {
-          fingerMotor.setPower(-0.2);
         } else if (gamepad1.dpad_down) {
           if (drivePower > 0.1) {
             drivePower += -0.1;
@@ -63,21 +51,7 @@ public class DriveMode extends LinearOpMode {
           drivePower = 1;
         } else if (gamepad1.a) {
           drivePower = 0.8;
-        } else if (gamepad2.right_bumper) {
-          wheelMotor.setPower(0.7);
-          sleep(1300);
-          wheelMotor.setPower(0.9);
-          sleep(1500);
-          wheelMotor.setPower(0);
-        } else if (gamepad2.left_bumper) {
-          wheelMotor.setPower(-0.7);
-          sleep(1300);
-          wheelMotor.setPower(-0.9);
-          sleep(1500);
-          wheelMotor.setPower(0);
         }
-        wheelMotor.setPower(-gamepad2.right_trigger);
-        wheelMotor.setPower(gamepad2.left_trigger);
         telemetry.update();
         telemetry.addData("Drive Factor", drivePower * 100);
       }
